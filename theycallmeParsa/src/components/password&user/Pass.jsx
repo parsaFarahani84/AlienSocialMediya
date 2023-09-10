065aba2c551d5e563600fa562ee3640e12bed616
Parsa Farahani <parsafarahani84@gmail.com>
2023-09-10 19:07:24 +0330

    ...

---
+++ theycallmeParsa/src/components/password&user/Pass.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Pass.css";
+
+function Pass() {
+  return (
+    <div>
+      <div className="pass-container">
+        <div className="pass-image"></div>
+        <div className="pass-login"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Pass;
--- theycallmeParsa/src/jotai/JotaiData.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { atomWithStorage } from "jotai/utils";
-
-const Atomdata = atomWithStorage("test", []);
-
-export { Atomdata };
